f88dc75a13060e4ca071b9dd3d3337c250c3ac15
twaldorf <trevor.automated@gmail.com>
2024-08-21 16:30:22 -0500

    update app

--- aaf-reader/src/app.js
+++ aaf-reader/src/app.js
@@ -1,11 +1,32 @@
-import './assets/style.css'; 
+import './assets/style.css';
+import dataurl from './assets/sample_summary_data.json';
+
+const grid = 500;
+var itr = 1;
 
 async function addSticky() {
-    const stickyNote = await miro.board.createStickyNote({
-        content: 'Hello, World!', 
-    }); 
+
+    dataurl.themes.map(async (e) => {
+        e.urls.map(async (url) => {
+            var todo = await miro.board.createStickyNote({
+                content: url,
+                x: itr * grid,
+                y: grid + 100,
+            })
+            itr++;
+        })
+        
+        var card = await miro.board.createCard({
+            title: e.name,
+            x: itr * grid,
+            y: grid,
+        }); 
+
+        await miro.board.viewport.zoomTo(card);
+    })
+
     
-    await miro.board.viewport.zoomTo(stickyNote); 
-} 
+    
+}
 
 addSticky(); 
